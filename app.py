01c4ff5063aa6ea525ea7cf144ec186c8d41cb09
Amit Gupta <streamlit@gprof.com>
2022-03-31 07:25:50 -0700

    Added patient data

--- app.py
+++ app.py
@@ -29,44 +29,22 @@ def processFile(uploadedFile):
     img_batch = np.expand_dims(img_array, axis=0)
     img_preprocessed = preprocess_input(img_batch)
     prediction_sample = new_model.predict(img_preprocessed)
-    st.title(prediction_sample)
     score_1 = prediction_sample[0][0]
     score_2 = prediction_sample[0][1]
-    return(prediction_sample[0])
     
     print(score_1, score_2)
-    #if (score_1 >= score_2):
-    #   predicted_label = 1
-    #else:
-    #  predicted_label = 2
-    #return predicted_label
-def get_prediction_localmodel(img_path):
-    # Please replace the brackets below with the location of your image which need to predict
-    img = image.load_img(img_path, target_size=IMG_SIZE)
-    img_array = image.img_to_array(img)
-    img_batch = np.expand_dims(img_array, axis=0)
-    img_preprocessed = preprocess_input(img_batch)
-    prediction_sample = new_model.predict(img_preprocessed)
-    #print(prediction_sample)
-    score_1 = prediction_sample[0][0]
-    score_2 = prediction_sample[0][1]
-    return(prediction_sample[0])
-    
-    #print(score_1, score_2)
-    #if (score_1 >= score_2):
-    #   predicted_label = 1
-    #else:
-    #  predicted_label = 2
-    #return predicted_label
-def fileAI():
-  print("About to get prediction")
-  l=get_prediction_localmodel('/Users/amitamit/Downloads/Cats_and_Dogs/Cats/cat_5.jpg')
-  print("Finished getting prediction",l)
+    if (score_1 >= score_2):
+       predicted_label = 1
+    else:
+      predicted_label = 2
+    return predicted_label,score_1,score_2
 def uploadAI():
-  st.title("Cats and Dogs")
+  st.title("File upload")
   uploadedFile=st.file_uploader("Choose file")
   if uploadedFile is not None:
-    processFile(uploadedFile)
-#fileAI()
-uploadAI()
+    (l,s1,s2)= processFile(uploadedFile)
+    return l
+# Main
+r=uploadAI()
+st.title(r)
 
